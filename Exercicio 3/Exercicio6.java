1f00d9c3053584360d5fe5c07521734c55b7a7c2
DevWayne <160737383+FelipeBastos2@users.noreply.github.com>
2024-03-17 00:00:01 -0300

    Add files via upload

---
+++ Exercicio 3/Exercicio6.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+        
+        System.out.println("Numero 1:");
+        System.out.println("Numero 2:");
+
+     
+      
+
+        Random aleatorio = new Random();
+        int num1 = aleatorio.nextInt();
+
+     
+        int num2 = aleatorio.nextInt();
+
+        int gerador = aleatorio.nextInt();
+       
+        
+
+        System.out.println("Numero sorteado:" +gerador);
+
+
+
+      
+    }
+    
+}
